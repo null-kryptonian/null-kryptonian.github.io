31091673e7823c96a7dcdb625d653b7713b8030e
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-10-13 13:58:24 +0600

    Update script.js

--- number-converter/script.js
+++ number-converter/script.js
@@ -15,20 +15,19 @@ $(document).on('click', '#convert-btn', function (e) {
 
     if(value1 === "Decimal" && value2 === "Binary") {
         var x = document.getElementById("x").value;
-        var binary = convertToBinary(x)
+        var binary = convertToBinary(x);
+        var onesComp = onesComplement(binary);
         document.getElementById("y").value = binary;
-        document.getElementById("y1").value = binary;
-        document.getElementById("y2").value = binary;
+        document.getElementById("y1").value = onesComp;
+        // document.getElementById("y2").value = binary;
     } else if(value1 === "Binary" && value2 === "Decimal") {
         var x = document.getElementById("x").value;
         var decimal = convertToDecimal(x)
         var binary = convertToBinary(x)
         document.getElementById("y").value = decimal;
-        document.getElementById("y1").value = binary;
-        document.getElementById("y2").value = binary;
+        // document.getElementById("y1").value = binary;
+        // document.getElementById("y2").value = binary;
     }
-    
-    // console.log(value1 + value2)
 });
 
 $(document).on('click', '#reset-btn', function (e) {
@@ -48,6 +47,27 @@ function convertToBinary(x) {
         i = i * 10;
     }
     return bin;
+}
+
+function onesComplement(x) {
+    let comp = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 10;
+        if(rem === 1) {
+            rem = 0;
+            rem *= i
+            comp += rem;
+            i *= 10;
+        } else {
+            rem = 1;
+            rem *= i
+            comp += rem;
+            i *= 10;
+        }
+        x = parseInt(x / 10);
+    }
+    return comp;
 }
 
 function convertToDecimal(x) {
